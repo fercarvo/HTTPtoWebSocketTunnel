8ec2a14ba1e52431d830957d319299540b6d30a4
Fernando Carvajal <efcu93@gmail.com>
2020-12-01 10:59:45 -0500

    se agrega funcionalidad de https

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 var http    = require('http');
+const https = require('https');
 var io      = require('socket.io-client')
 var Stream  = require('stream').Transform
+var protocolo = http;
+
 
 var proxy_server = process.argv[2]
 if (!proxy_server) throw Error(`Proxy server not defined: ${proxy_server}`)
@@ -9,18 +12,25 @@ var hub = io(proxy_server, {path: '/connection_session'});
 
 hub.on('request', async (req, response_cb) => {
     //console.log(req.method + ' ' + req.url);
-    var data = await ajax(req.hostname, req.port, req.url, req.method, req.headers, req.data)
+    var data = await ajax(req.hostname, req.port, req.url, req.method, req.headers, req.data, req.https)
     response_cb(data)
 })
 
 hub.on('connect', () => console.log(`This <-----> ${proxy_server} [${new Date().toLocaleString()}]`))
 hub.on('disconnect', () => console.log(`This <-   -> ${proxy_server} [${new Date().toLocaleString()}]`))
 
-function ajax (hostname, port, path, method, headers, data) {
+function ajax (hostname, port, path, method, headers, data, coneccionHttps) {
     const options = { hostname, port, path, method, headers }
 
     return new Promise(resolve => {
-        var req = http.request(options, res => {
+        if (coneccionHttps) {
+            process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+            protocolo = https;
+        } else {
+            protocolo = http;
+        }
+        
+        var req = protocolo.request(options, res => {
             var payload = new Stream();     
 
             res.on('data', chunk => payload.push(chunk))
